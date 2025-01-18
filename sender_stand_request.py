5d018a4ecf55925b5a5ae51b72e260cfa9d9995e
Alexandr Popov <pak100500@mail.ru>
2025-01-18 16:30:50 +0400

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,55 @@
+
+# Импорт настроек из модуля configuration, который содержит параметры конфигурации, такие как URL сервиса
+import configuration
+
+# Импорт библиотеки requests для выполнения HTTP-запросов
+import requests
+
+# Импорт данных запроса из модуля data, в котором определены заголовки и тело запроса
+import data
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+# Вызов функции post_new_user с телом запроса для создания нового пользователя из модуля data
+response = post_new_user(data.user_body)
+
+# Вызов функции post_new_user с телом запроса для создания нового пользователя из модуля data
+response = post_new_user(data.user_body)
+
+# Определение функции для отправки POST-запроса на поиск наборов по продуктам
+def post_products_kits(products_ids):
+    # Отправка POST-запроса с использованием URL из конфигурации, данных о продуктах и заголовков
+    # Возвращается объект ответа, полученный от сервера
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,  # Тело запроса содержит ID продуктов в формате JSON
+                         headers=data.headers)  # Использование заголовков из файла data.py
+
+# Вызов функции с передачей списка ID продуктов из файла data.py
+response = post_products_kits(data.products_ids)
+
+
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+# Вызов функции post_new_user с телом запроса для создания нового пользователя из модуля data
+response = post_new_user(data.user_body)
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+response = get_users_table() 
